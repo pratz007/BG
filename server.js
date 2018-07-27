83748cb89d13d8c126121539e1a2cc63f888fbcc
root <root@ip-10-0-1-75.ec2.internal>
2018-07-27 19:29:12 +0000

    server

---
+++ server.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env nodejs
+var http = require('http');
+var httpdispatcher = require ('httpdispatcher');
+var dispatcher = new httpdispatcher();
+
+
+const PORT = 8080;
+
+
+function handleRequest(request, response){
+    try {
+        
+        console.log(request.url);
+        
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+var myFirstServer = http.createServer(handleRequest);
+
+
+
+
+dispatcher.onGet("/", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Hey, this is the homepage of your Blue server</h1>');
+});
+
+dispatcher.onGet("/welcome", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Welcome homepage for Blue');
+});
+
+dispatcher.onError(function(req, res) {
+    res.writeHead(404);
+    res.end("Error, the URL doesn't exist");
+});
+
+
+myFirstServer.listen(PORT, function(){
+
+    console.log("Blue listening on: http://localhost:%s", PORT);
+});
+
