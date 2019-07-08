1520488dfa5190b639a893409b419580d91b96bc
pratz007 <groverprateek07@gmail.com>
2019-07-08 13:38:37 +0530

    Release test

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ dispatcher.onGet("/", function(req, res) {
 
 dispatcher.onGet("/welcome", function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Welcome homepage for Updated Server-#1/PR');
+    res.end('Welcome homepage for Updated Server-#1/RELEASE');
 });
 
 dispatcher.onError(function(req, res) {
